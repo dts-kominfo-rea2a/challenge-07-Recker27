6bb7a0a685bb21f7003dc01d618c1f9bbdc619f1
MUHAMMAD FIKRI <817933290@intranet.pajak.go.id>
2022-06-13 16:07:39 +0700

    initial commit challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const outputTheaterIXX = await promiseTheaterIXX();
+    const outputTheaterVGC = await promiseTheaterVGC();
+    const arrOutput = outputTheaterIXX.concat(outputTheaterVGC);
+
+    return arrOutput.filter((record) => record.hasil === emosi).length;
+  } catch (err) {
+    return `Error: ${err}`;
+  }
+};
 
 module.exports = {
   promiseOutput,
